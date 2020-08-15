361ae8f5d20d0ad41f4fadd7541462bc6af5850a
grantwilliams <grantmw@uw.edu>
2020-08-15 15:50:26 -0700

    Added mapping of openings.

--- components/MiniEvent.tsx
+++ components/MiniEvent.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, TouchableOpacity } from 'react-native';
 
 import { MonoText } from './StyledText';
-import { View } from './Themed';
+import { Text, View } from './Themed';
 import MiniProfile from './MiniProfile';
 
 /**
@@ -15,8 +15,16 @@ export default function MiniEvent({uid, displayUser=true}: {uid: string, display
   const sampleEvent = {
     title: 'Fun event!',
     openings: 5,
-    slots: ['🔥', '🔥', '🔥', '', '']
+    slots: ['🔥', '🔥', '🔥', '', '',]
   };
+
+  const OpeningsSlots = sampleEvent.slots.map((slot)=> {
+    return (
+      <View style={styles.openingsSlot}>
+        <Text>{slot}</Text>
+      </View>
+    );
+  });
 
   if (displayUser) {
     return ( 
@@ -28,10 +36,10 @@ export default function MiniEvent({uid, displayUser=true}: {uid: string, display
         </View>
         <View style={styles.separator}/>
         <View style={styles.openingsContainer}>
-          <MonoText style={styles.openingsText}>openings: </MonoText>
-          {sampleEvent.slots.map(slot => {
-            
-          })}
+          <MonoText style={styles.openingsText}>openings:</MonoText>
+          <View style={styles.openingsSlotContainer}>
+            {OpeningsSlots}
+          </View>
         </View>
       </TouchableOpacity>
     );
@@ -43,10 +51,10 @@ export default function MiniEvent({uid, displayUser=true}: {uid: string, display
         </View>
         <View style={styles.separator}/>
         <View style={styles.openingsContainer}>
-          <MonoText style={styles.openingsText}>openings: </MonoText>
-          {sampleEvent.slots.map(slot => {
-            
-          })}
+          <MonoText style={styles.openingsText}>openings:</MonoText>
+          <View style={styles.openingsSlotContainer}>
+            {OpeningsSlots}
+          </View>
         </View>
       </TouchableOpacity>
     );
@@ -66,10 +74,34 @@ const styles = StyleSheet.create({
   },
   openingsContainer: {
     backgroundColor: 'white',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 2, 
+    flexWrap: 'wrap',
   },
   openingsText: {
     fontSize: 16,
     color: 'black',
+  },
+  openingsSlot: {
+    backgroundColor: 'white',
+    width: 30,
+    height: 30,
+    borderWidth: 1,
+    borderColor: 'red',
+    borderRadius: 20,
+    padding: 5,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 2,
+  },
+  openingsSlotContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    flexWrap: 'wrap',
   },
   separator: {
     height: 2,
