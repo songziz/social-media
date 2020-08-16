a6ba9aa54877de943284a8c3779d2756ea37bc76
wesleyplam <67298266+wesleyplam@users.noreply.github.com>
2020-08-16 10:54:42 -0700

    sample users

---
+++ assets/SampleUsers.tsx
@@ -0,0 +1,27 @@
+export default function SampleUsers() {
+
+
+    const user1 = {
+        username: 'we$money',
+        icon: '💸',
+        uid: '3LcSVSL7VhPmaQugBS38xUBWl712',
+    }
+
+    const user2 = {
+        username: 'kingHenry',
+        icon: '👑',
+        uid: 'fdj9FJ9eldkdfdn7dfj3Jd83jJfe',
+    }
+
+    const user3 = {
+        username: 'breezy',
+        icon: '💨',
+        uid: 'fgINdodme3kd93dj92jd245djFw',
+    }
+
+    const user4 = {
+        username: 'holderOfTime',
+        icon: '⏱️',
+        uid: 'dfdfFIJFeod3r0f3mfod0fFOe4F',
+    }
+} 
